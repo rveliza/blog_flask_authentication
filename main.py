ce0b4bd8312791c295acc80f294c465c4839290c
Reyner Veliz <reyner@reynerveliz.com>
2024-02-28 13:36:51 -0600

    Add gravatars

--- main.py
+++ main.py
@@ -38,6 +38,17 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
+# Gravatar 
+gravatar = Gravatar(app,
+                    size=100,
+                    rating='g',
+                    default='retro',
+                    force_default=False,
+                    force_lower=False,
+                    use_ssl=False,
+                    base_url=None)
+
+
 # Configure Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
