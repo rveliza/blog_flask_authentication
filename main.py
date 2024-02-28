2ce89624ff182809c0a87c25ff424f683af0439d
Reyner Veliz <reyner@reynerveliz.com>
2024-02-28 08:04:58 -0600

    Create, Edit and Delete buttons only visible if current_user.id == 1

--- main.py
+++ main.py
@@ -119,9 +119,9 @@ def login():
         elif not check_password_hash(user.password, password):
             flash('Password incorrect, please try again.')
             return redirect(url_for('login'))
-
         else:
             login_user(user)
+            return redirect(url_for('get_all_posts'))
     return render_template("login.html", form=form)
 
 
